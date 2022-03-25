ab2d7e7f35046a350598e818f532b0654768a4c6
Rodolfo Morales <rodolfomlphx@gmail.com>
2022-03-24 21:42:05 -0700

    first commit

---
+++ mynotes/src/assets/data.js
@@ -0,0 +1,19 @@
+let notes = [
+    {
+        "id": 1,
+        "body": "Todays Agenda\n\n- Walk Dog\n- Feed fish\n- Play basketball\n- Eat a salad",
+        "updated": "2021-07-14T13:49:02.0786532",
+    },
+    {
+        "id": 2,
+        "body": "Bob from bar down the \n\n- Take out trash\n- Eat food",
+        "updated": "2021-07-13T20:43:18:5500582",
+    },
+    {
+        "id":3,
+        "body": "Wash Car",
+        "updated": "2021-07-13T19:46:12.1873062",
+    }
+]
+
+export default notes;
